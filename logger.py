2e8f357f8982f8b58732faa180a11c9f0f808bfb
Jan C. Brammer <jan.c.brammer@gmail.com>
2021-02-05 13:50:47 +0100

    Catch connection errors.

--- logger.py
+++ logger.py
@@ -10,7 +10,7 @@ class RedisLogger(QObject):
         super().__init__()
 
         self.model = model
-        self.redis = redis.Redis()
+        self.redis = redis.Redis(REDIS_HOST, REDIS_PORT)    # connection to server is not established at instantiation, but once first command to server is issued (i.e., first publish() call)
 
         self.model.ibis_buffer_update.connect(self.publish)
         self.model.mean_hrv_update.connect(self.publish)
@@ -19,17 +19,16 @@ class RedisLogger(QObject):
         self.model.hrv_target_update.connect(self.publish)
         self.model.biofeedback_update.connect(self.publish)
 
-    def connect():
-        pass
-
     def publish(self, value):
         key, val = value
         if isinstance(val, (list, np.ndarray)):
             val = val[-1]
         if isinstance(val, np.int32):
             val = int(val)
-        print(key, val)
-        self.redis.set(key, val)
+        try:
+            self.redis.publish(key, val)
+        except redis.exceptions.ConnectionError as e:
+            print(e)    # client (re)-connects automatically; as soon as server is back up (in case of previous outage) client-server communication resumes
 
     def record():
         pass
