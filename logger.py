5cb247221bdd2aeae32891cabb25d7b574e6a13f
Jan C. Brammer <jan.c.brammer@gmail.com>
2021-04-16 14:15:53 +0200

    Abort recording if user does not specify file name.

--- logger.py
+++ logger.py
@@ -4,7 +4,6 @@ import threading
 import traceback
 import numpy as np
 from datetime import datetime
-from pathlib import Path
 from config import REDIS_HOST, REDIS_PORT
 from PySide2.QtCore import QObject, Signal
 from PySide2.QtWidgets import QFileDialog
@@ -25,8 +24,8 @@ class RedisPublisher(QObject):
                 self.redis.ping()    # raises if server is down
                 self.connected = True
             except redis.exceptions.ConnectionError as e:
-                print("Redis server is down.")
                 self.connected = False
+                print("Redis server is down.")
             time.sleep(5)
 
     def publish(self, value):
@@ -58,13 +57,19 @@ class RedisLogger(QObject):
         threading.excepthook = self._handle_redis_exceptions
 
     def start_recording(self):
+        if self.subscription_thread is not None:
+            print("Already subscribed.")
+            return    # don't re-subscribe
+        if self.file:
+            print(f"Already writing to a file at {self.file.name}.")
+            return    # only write to one file at a time
         subscribed = self._subscribe()
         if not subscribed:
             return
-        if self.file:
-            print(f"Already writing to a file at {self.file.name}.")
+        self.file = self._open_file()    # subscription_thread is already running and starts writing to file as soon as the latter is instantiated
+        if self.file is None:    # user cancelled or closed file dialogue
+            self._close_subscription()
             return
-        self.file = self._open_file()    # subscription_thread is already running and starts writing to wfile
         with threading.Lock():    # prevent subscription_thread from writing to file while writing header
             self.file.write("event\tvalue\ttimestamp\n")    # header
         self.recording_status.emit(0)
@@ -82,11 +87,11 @@ class RedisLogger(QObject):
     def _close_subscription(self):
         if not self.subscription_thread:
             return
-        print("Closing subscription thread.")
         self.subscription_thread.stop()
         self.subscription_thread = None
         self.subscription.punsubscribe()
         self.subscription.close()    # terminates connection to Redis server
+        print("Closed subscription thread.")
 
     def _close_file(self):
         if not self.file:
@@ -102,14 +107,12 @@ class RedisLogger(QObject):
 
     def _subscribe(self):
         subscribed = False
-        if self.subscription_thread is not None:
-            print("Already subscribed.")
-            return subscribed    # don't re-subscribe
         try:
             self.subscription.psubscribe(**{"*": self._write_to_file})    # subscribe to all channels by matching everything; instantiates connection to Redis server
             self.subscription_thread = self.subscription.run_in_thread(sleep_time=0.001)     # Redis connection exceptions are handled with threading.excepthook
             subscribed = True
         except redis.exceptions.ConnectionError as e:
+            self.subscription_thread = None
             print("Couldn't subscribe.")
             print(e)
         return subscribed
@@ -117,21 +120,22 @@ class RedisLogger(QObject):
     def _write_to_file(self, data):
         if not self.file:
             return
-        print(f"Logging: {data}.")
         if data["type"] != "pmessage":
             return
         key = data["channel"]
         val = data["data"]
         timestamp = datetime.now().isoformat()
         self.file.write(f"{key}\t{val}\t{timestamp}\n")
+        print(f"Logged: {key}\t{val}\t{timestamp}\n.")
 
     @staticmethod
     def _open_file():
+        file = None
         current_time = datetime.now().strftime("%Y.%m.%d.%H.%M.%S")
-        default_file_name = f"sub-?_day-?_task-?_time-{current_time}"
+        default_file_name = f"sub-?_day-?_task-?_time-{current_time}"    # question marks are invalid characters for file names on Windows and hence force user to specify file name
         save_path = QFileDialog.getSaveFileName(None, "Create file",
                                                 default_file_name)[0]
         if not save_path:    # user cancelled or closed file dialog
-            save_path = str(Path.cwd().joinpath(default_file_name))
+            return file
         file = open(f"{save_path}.tsv", "a+")
         return file
