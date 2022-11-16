2beea3f10dbb9b0610ca28e9f0e28883d4a58358
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-11-16 16:35:18 +0000

    Specify dependencies in pyproject.toml

--- .devcontainer/install_dependencies.sh
+++ .devcontainer/install_dependencies.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-pip install PySide6 numpy
+# install dependencies specified in pyproject.toml
+pip install --upgrade pip
+pip install -e .[dev]
 
-# install missing dependencies
+# install missing OS dependencies
 sudo apt-get update && \
 sudo apt-get install -y libegl-dev libxkbcommon-x11-0 libdbus-1-dev '^libxcb.*-dev'
