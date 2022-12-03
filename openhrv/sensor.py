fdd9e774b3cf5ff7728019fe64fc9c541a398e30
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-12-03 16:54:34 +0000

    Remove Bluetooth-related workaround
    https://bugreports.qt.io/browse/QTBUG-80770

--- openhrv/sensor.py
+++ openhrv/sensor.py
@@ -82,9 +82,7 @@ class SensorClient(QObject):
         self.client = QLowEnergyController.createCentral(sensor)
         self.client.errorOccurred.connect(self._catch_error)
         self.client.connected.connect(self._discover_services)
-        self.client.discoveryFinished.connect(
-            self._connect_hr_service, Qt.QueuedConnection
-        )
+        self.client.discoveryFinished.connect(self._connect_hr_service)
         self.client.disconnected.connect(self._reset_connection)
         self.client.connectToDevice()
 
